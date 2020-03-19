60473b296553725476106499404dc4bbbdf1878b
Dionis Oros <dionis.oros@complyadvantage.com>
2020-03-19 13:59:18 +0200

    adding cypress for e2e + login (valid credentials) e2e test

---
+++ address-book-client/cypress/support/selectors/login_selectors.js
@@ -0,0 +1,4 @@
+export const LOGIN_USERNAME_INPUT = '#username';
+export const LOGIN_PASSWD_INPUT = '#password';
+export const VALID_USERNAME = 'usernametest';
+export const VALID_PASSWORD = 'password';
