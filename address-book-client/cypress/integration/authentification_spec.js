77f530ab3532c156c4325398115691feeb0dc6c0
Dionis Oros <dionis.oros@complyadvantage.com>
2020-03-19 14:01:41 +0200

    command npm run e2e

---
+++ address-book-client/cypress/integration/authentification_spec.js
@@ -0,0 +1,34 @@
+import {
+  LOGIN_PASSWD_INPUT,
+  LOGIN_USERNAME_INPUT,
+  VALID_USERNAME,
+  VALID_PASSWORD
+} from "../support/selectors/login_selectors";
+
+describe('Testing the Sign-in/Sign-up page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200');
+  });
+
+  it('should see Sign In, Sign Up, Hello, Friend! at first', () => {
+    cy.contains('Hello, Friend!');
+    cy.contains('Sign in');
+    cy.contains('Sign up');
+    cy.get(LOGIN_USERNAME_INPUT).type(VALID_USERNAME);
+    cy.get(LOGIN_PASSWD_INPUT).type(VALID_PASSWORD);
+    cy.get('#signIn').click();
+  });
+
+  function validLogin() {
+    cy.get(LOGIN_USERNAME_INPUT).type(VALID_USERNAME);
+    cy.get(LOGIN_PASSWD_INPUT).type(VALID_PASSWORD);
+    cy.get('#signIn').click();
+  }
+
+  it('should login in the app and see the success message & contacts page', () => {
+    validLogin();
+    cy.contains('Success!');
+    cy.contains('Logged in.');
+    cy.contains('Contacts');
+  });
+});
